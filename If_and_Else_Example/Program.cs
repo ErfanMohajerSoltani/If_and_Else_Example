cd5eaf0c785cb64782990b5322c789b1ad2d7660
Erfan Mohajer Soltani <erfanm512@gmail.com>
2022-02-28 01:49:48 +0330

    using if, else if and else command

--- If_and_Else_Example/Program.cs
+++ If_and_Else_Example/Program.cs
@@ -6,7 +6,32 @@ namespace If_and_Else_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int firstNumber = int.Parse(Console.ReadLine());
+                int secondNumber = int.Parse(Console.ReadLine());
+
+                // use if to set some condition 
+                if (firstNumber == secondNumber)
+                {
+                    Console.WriteLine("1st number and 2nd number are equal to eachother");
+                }
+                // use else if for that time your previous condition or conditions wasn't true and you want to have another condition
+                else if (firstNumber > secondNumber)
+                {
+                    Console.WriteLine("1st number is more than 2nd number");
+                }
+                // use else for that time none of your previous conditions weren't true and you don't want to set new condition
+                else
+                {
+                    Console.WriteLine("none of previous conditions weren't ture");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("ERROR!!!");
+            }
         }
     }
 }
